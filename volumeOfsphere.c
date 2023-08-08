36721249193997cf3f0f12a3cbe0a0a5bbc9d21e
Atique Kondvilkar <90702732+AtiqueAK@users.noreply.github.com>
2023-08-08 16:46:21 +0530

    Update volumeOfsphere.c

--- volumeOfsphere.c
+++ volumeOfsphere.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main(){
-    float r = 5;
+    float r;
+    printf("Enter the radius of sphere :");
+    scanf("%d",&r);
     float v = 4 * 3.14 * r * r * r / 3;
     printf("The volume is : %f", v);
 
